19a532ee1d9e86b895b7124b2114f7c37a5fd13c
kumarguptavaibhav <vaibhav9161porwal@gmail.com>
2025-02-12 19:21:08 +0530

    Update base url in frontend

--- frontend/src/redux/apiSlice.js
+++ frontend/src/redux/apiSlice.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://real-time-event-management.onrender.com",
+  }),
   endpoints: (builder) => ({
     createUser: builder.mutation({
       query: (payload) => ({
